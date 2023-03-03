091ec80b34cf53f0c3b6d8504b71aece12133571
AungMyintKo <aungmyintkoanarmala@gmail.com>
2023-03-03 13:03:03 +0630

    adding compressionp

--- src/index.js
+++ src/index.js
@@ -2,14 +2,16 @@ import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
+import compression from "compression";
 import ApiRoutes from "../routes/api.routes.js";
 import cors from "cors";
 const app = express();
 dotenv.config();
 
 //Middlewares
-app.use(express.json());
-app.use(express.urlencoded());
+app.use(express.json({ limit: "50mb" }));
+app.use(express.urlencoded({ limit: "50mb", extended: true }));
+app.use(compression());
 app.use(cookieParser());
 app.use(
   cors({
